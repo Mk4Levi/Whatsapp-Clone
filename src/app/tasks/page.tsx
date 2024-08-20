db5a409695c1c6cc0b59dac66d564a157097c374
Manthan <mksoul1811@gmail.com>
2024-08-20 19:34:29 +0530

    Fix bug in Tasks page

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,28 +1,46 @@
 "use client";
 
-import { useMutation, useQuery } from "convex/react";
-import { api } from "../../../convex/_generated/api";
+import { useState } from "react";
+
+// import { useMutation, useQuery } from "convex/react";
+// import { api } from "../../../convex/_generated/api";
+
+// Define the type for a task object
+interface Task {
+  _id: string;
+  text: string;
+}
+
+// Initial demo tasks data
+const initialTasks = [
+  { _id: "task1", text: "Learn Next.js" },
+  { _id: "task2", text: "Build a Convex App" },
+  { _id: "task3", text: "Deploy to Vercel" },
+];
 
 const TasksPage = () => {
-	const tasks = useQuery(api.tasks.getTasks);
-	const deleteTask = useMutation(api.tasks.deleteTask);
+  // const tasks = useQuery(api.tasks.getTasks);
+  // const deleteTask = useMutation(api.tasks.deleteTask);
 
-	return (
-		<div className='p-10 flex flex-col gap-4'>
-			<h1 className='text-5xl'> All tasks are here in real-time</h1>
-			{tasks?.map((task) => (
-				<div key={task._id} className='flex gap-2'>
-					<span>{task.text}</span>
-					<button
-						onClick={async () => {
-							await deleteTask({ id: task._id });
-						}}
-					>
-						Delete Task
-					</button>
-				</div>
-			))}
-		</div>
-	);
+  const [tasks, setTasks] = useState<Task[]>(initialTasks);
+
+  // Simulate deleteTask mutation
+  const deleteTask = (id: string) => {
+    // Update the tasks state to remove the task with the given id
+    setTasks((prevTasks) => prevTasks.filter((task) => task._id !== id));
+  };
+
+  return (
+    <div className="p-10 flex flex-col gap-4">
+      <h1 className="text-5xl">All tasks are here in real-time</h1>
+      {tasks?.map((task) => (
+        <div key={task._id} className="flex gap-2">
+          <span>{task.text}</span>
+          <button onClick={() => deleteTask(task._id)}>Delete Task</button>
+        </div>
+      ))}
+    </div>
+  );
 };
+
 export default TasksPage;
