48953269d916beabe522bf736dfb65ea17a0d02e
Manthan <mksoul1811@gmail.com>
2024-08-20 17:02:39 +0530

    Fix major bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
 import LeftPanel from "@/components/home/left-panel";
 import RightPanel from "@/components/home/right-panel";
 import { useTheme } from "next-themes";
+import Head from "next/head";
 
 export default function Home() {
-	return (
-		<main className='m-5'>
-			<div className='flex overflow-y-hidden h-[calc(100vh-50px)] max-w-[1700px] mx-auto bg-left-panel'>
-				{/* Green background decorator for Light Mode */}
-				<div className='fixed top-0 left-0 w-full h-36 bg-green-primary dark:bg-transparent -z-30' />
-				<LeftPanel />
-				<RightPanel />
-			</div>
-		</main>
-	);
+  return (
+    <main className="m-5">
+      <Head>
+        <title>Whatsapp Clone</title>
+        <meta name="description" content="Instagram Clone built with Next.js" />
+      </Head>
+      <div className="flex overflow-y-hidden h-[calc(100vh-50px)] max-w-[1700px] mx-auto bg-left-panel">
+        {/* Green background decorator for Light Mode */}
+        <div className="fixed top-0 left-0 w-full h-36 bg-green-primary dark:bg-transparent -z-30" />
+        <LeftPanel />
+        <RightPanel />
+      </div>
+    </main>
+  );
 }
